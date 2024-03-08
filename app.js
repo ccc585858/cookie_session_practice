246a8bc839b0b8887173522d94843cd677e85072
Pineapple <ccc585858@gmail.com>
2024-03-08 22:59:49 +0800

    signedCookies practice

--- app.js
+++ app.js
@@ -2,19 +2,23 @@ const express = require("express");
 const app = express();
 const cookieParser = require("cookie-parser");
 
-app.use(cookieParser());
+app.use(cookieParser("Kazuha"));
 
 app.get("/", (req, res) => {
   res.send("這是首頁");
 });
 
 app.get("/setCookie", (req, res) => {
-  res.cookie("yourCookie", "Oreo");
+  //   res.cookie("yourCookie", "Oreo");
+  res.cookie("yourCookie", "Oreo", { signed: true });
   return res.send("已經設置 Cookie...");
 });
 
 app.get("/seeCookie", (req, res) => {
-  return res.send("看一下已經設定好的 Cookie..." + req.cookies.yourCookie);
+  console.log(req.signedCookies);
+  return res.send(
+    "看一下已經設定好的 Cookie..." + req.signedCookies.yourCookie
+  );
 });
 
 app.listen(3000, () => {
