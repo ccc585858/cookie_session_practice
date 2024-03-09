3fc31e3d646050d3cdda9780ea4f1d8a53bf706b
Pineapple <ccc585858@gmail.com>
2024-03-09 16:46:31 +0800

    install express-session and session practice

--- app.js
+++ app.js
@@ -1,8 +1,25 @@
 const express = require("express");
 const app = express();
 const cookieParser = require("cookie-parser");
+const session = require("express-session");
 
 app.use(cookieParser("Kazuha"));
+app.use(
+  session({
+    secret: "Kazuha 和 Sakura 誰是日本人?",
+    resave: false,
+    saveUninitialized: false,
+    cookie: { secure: false }, // 在 loaclhost 上面跑，沒有 https
+  })
+);
+
+const checkUser = (req, res, next) => {
+  if (!req.session.isVerified) {
+    return res.send("請先登入系統，才能看到秘密。");
+  } else {
+    next();
+  }
+};
 
 app.get("/", (req, res) => {
   res.send("這是首頁");
@@ -19,6 +36,36 @@ app.get("/seeCookie", (req, res) => {
   return res.send(
     "看一下已經設定好的 Cookie..." + req.signedCookies.yourCookie
   );
+});
+
+// 設定 session
+app.get("/setSessionData", (req, res) => {
+  // console.log(req.session);
+  req.session.example = "something not important...";
+  return res.send(
+    "在伺服器上設置 session 資料，在瀏覽器上設置簽名後的 session id"
+  );
+});
+
+// 取的 session 的資料
+app.get("/seeSessionData", (req, res) => {
+  console.log(req.session);
+  return res.send("看一下已經設定好的 Session 資料");
+});
+
+app.get("/verifyUser", (req, res) => {
+  req.session.isVerified = true;
+  return res.send("驗證成功...");
+});
+
+// 模擬登入的狀態
+// 不管是 secret 還是 secret2 這個 route，都會執行 checkUser middleware。
+app.get("/secret", checkUser, (req, res) => {
+  return res.send("秘密是，Momo");
+});
+
+app.get("/secret2", checkUser, (req, res) => {
+  return res.send("秘密2，Kazuha");
 });
 
 app.listen(3000, () => {
